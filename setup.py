0bea713ab85f1e30dcb5f6ca9e907e47d830416a
__sp__ <shyuep@gmail.com>
2019-02-08 14:38:53 -0800

    Update version.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='megnet',
-    version='0.0.2',
+    version='0.0.3',
     decription='MatErials Graph Networks for machine learning of molecules and crystals.',
     long_description=long_description,
     long_description_content_type='text/markdown',
