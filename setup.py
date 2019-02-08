6c6f131144731814e7828dc1c3eae1bfdc4e0bbf
__sp__ <shyuep@gmail.com>
2019-02-08 13:17:26 -0800

    Add README.

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup
 from setuptools import find_packages
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='megnet',
-    version='0.0.1',
+    version='0.0.2',
     decription='MatErials Graph Networks for machine learning of molecules and crystals.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Chi Chen',
     author_email='chc273@eng.ucsd.edu',
     download_url='https://github.com/materialsvirtuallab/megnet',
@@ -12,7 +19,6 @@ setup(
     install_requires=['keras', 'numpy', 'tensorflow', "scikit-learn", 'pymatgen'],
     extras_require={
         'model_saving': ['h5py'], },
-    package_data={'megnet': ['README.md']},
     package=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
