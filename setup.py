6e5f2f657ad88f366957f0c9f68191d0c624886a
__sp__ <shyuep@gmail.com>
2019-02-10 12:23:37 -0800

    Cleanup.

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
-from setuptools import setup
-from setuptools import find_packages
-
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+from os.path import dirname, abspath, join
+this_directory = abspath(dirname(__file__))
+with open(join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
