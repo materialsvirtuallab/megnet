15ee967e0d2ec66d5af7c3f55422c663a355628a
__sp__ <shyuep@gmail.com>
2019-04-08 13:03:06 -0700

    Minor change to setup.py.

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 from setuptools import find_packages
-
 from os.path import dirname, abspath, join
 this_dir = abspath(dirname(__file__))
+
 with open(join(this_dir, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
@@ -16,7 +16,8 @@ setup(
     author_email='chc273@eng.ucsd.edu',
     download_url='https://github.com/materialsvirtuallab/megnet',
     license='BSD',
-    install_requires=['keras', 'numpy', 'tensorflow', "scikit-learn", 'pymatgen', 'monty'],
+    install_requires=['keras', 'numpy', 'tensorflow', "scikit-learn",
+                      'pymatgen', 'monty'],
     extras_require={
         'model_saving': ['h5py'], },
     package=find_packages(),
@@ -24,6 +25,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
