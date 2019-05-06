47770ee69d018a77b838484c9a84304ab9d98d1c
__sp__ <shyuep@gmail.com>
2019-05-06 15:23:19 -0700

    Proper release of megnet for PyPI. Fixes #4.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(join(this_dir, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='megnet',
-    version='0.2.2',
+    version='0.3.0',
     decription='MatErials Graph Networks for machine learning of molecules and crystals.',
     long_description=long_description,
     long_description_content_type='text/markdown',
