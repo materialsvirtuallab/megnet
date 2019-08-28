29441bb217d0585ce0b2397dca4d8b95a851e4c0
Chi Chen <chc273@eng.ucsd.edu>
2019-08-28 11:41:36 -0700

    fix tests

--- megnet/utils/tests/test_general.py
+++ megnet/utils/tests/test_general.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 
-from megnet.utils.general import expand_1st, to_list
+from megnet.utils.general import expand_1st, to_list, fast_label_binarize
 
 
 class TestGeneralUtils(unittest.TestCase):
@@ -14,8 +14,15 @@ class TestGeneralUtils(unittest.TestCase):
         y = [1]
         z = tuple([1, 2, 3])
         v = np.array([1, 2, 3])
-        for k in [x, y, z, v]:
+        k = np.array([[1, 2], [3, 4]])
+        for k in [x, y, z, v, k]:
             self.assertTrue(type(to_list(k)), list)
+
+    def test_fast_label_binarize(self):
+        binaries = fast_label_binarize(1, [0, 1])
+        self.assertListEqual(binaries, [0])
+        binaries = fast_label_binarize(1, [0, 1, 2])
+        self.assertListEqual(binaries, [0, 1, 0])
 
 
 if __name__ == "__main__":
