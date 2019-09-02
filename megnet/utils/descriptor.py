713cc821a004d99eb954e91bd825ef9651b1538c
zyrd <shyuep@gmail.com>
2019-09-02 16:03:58 -0700

    Minor cleanup.

--- megnet/utils/descriptor.py
+++ megnet/utils/descriptor.py
@@ -7,7 +7,6 @@ import os
 from megnet.models import MEGNetModel, GraphModel
 from keras.models import Model
 
-
 DEFAULT_MODEL = os.path.join(os.path.dirname(__file__), '../../mvl_models/mp-2019.4.1/formation_energy.hdf5')
 
 
