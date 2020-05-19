a574622e3c9f4b8d7944291148832d76b9cc643e
Chi Chen <chc273@eng.ucsd.edu>
2020-05-19 16:41:07 -0700

    add datatypes

---
+++ megnet/utils/data_types.py
@@ -0,0 +1,48 @@
+"""Data types"""
+import numpy as np
+import tensorflow as tf
+
+
+DTYPES = {'float32': {'numpy': np.float32, 'tf': tf.float32},
+          'float16': {'numpy': np.float16, 'tf': tf.float16},
+          'int32': {'numpy': np.int32, 'tf': tf.int32},
+          'int16': {'numpy': np.int32, 'tf': tf.int32}}
+
+
+class DataType:
+    np_float = np.float32
+    np_int = np.int32
+    tf_float = tf.float32
+    tf_int = tf.int32
+
+    @classmethod
+    def set_dtype(cls, data_type: str) -> None:
+        """
+        Class method to set the data types
+        Args:
+            data_type (str): '16' or '32'
+        """
+        if data_type.endswith('32'):
+            float_key = 'float32'
+            int_key = 'int32'
+        elif data_type.endswith('16'):
+            float_key = 'float16'
+            int_key = 'int16'
+        else:
+            raise ValueError("Data type not known, choose '16' or '32'")
+
+        cls.np_float = DTYPES[float_key]['numpy']
+        cls.tf_float = DTYPES[float_key]['tf']
+        cls.np_int = DTYPES[int_key]['numpy']
+        cls.tf_int = DTYPES[int_key]['tf']
+
+
+def set_global_dtypes(data_type) -> None:
+    """
+    Function to set the data types
+    Args:
+        data_type (str): '16' or '32'
+    Returns:
+
+    """
+    DataType.set_dtype(data_type)
---
+++ megnet/utils/tests/test_dtypes.py
@@ -0,0 +1,32 @@
+"""
+Test the data types
+"""
+import unittest
+
+import numpy as np
+import tensorflow as tf
+
+from megnet.utils.data_types import DataType, set_global_dtypes
+
+
+class TestDataType(unittest.TestCase):
+    def test_set_16(self):
+        DataType.set_dtype("16")
+        self.assertTrue(DataType.np_int, np.int16)
+        self.assertTrue(DataType.np_float, np.float16)
+        self.assertTrue(DataType.tf_int, tf.int16)
+        self.assertTrue(DataType.tf_float, tf.float16)
+        set_global_dtypes("32")
+        self.assertTrue(DataType.np_int, np.int32)
+
+    def test_set_32(self):
+        DataType.set_dtype("32")
+        self.assertTrue(DataType.np_int, np.int32)
+        self.assertTrue(DataType.np_float, np.float32)
+
+    def test_wrong(self):
+        self.assertRaises(ValueError, DataType.set_dtype, "64")
+
+
+if __name__ == "__main__":
+    unittest.main()
