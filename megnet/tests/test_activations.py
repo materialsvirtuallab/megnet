355ac68d0437245f38e95f04bacdd1fcf222dc8a
Chi Chen <chc273@eng.ucsd.edu>
2021-03-01 11:04:13 -0800

    add swish activation function

--- megnet/tests/test_activations.py
+++ megnet/tests/test_activations.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import unittest
-from megnet.activations import softplus2
+from megnet.activations import softplus2, swish
 import numpy as np
 
 
@@ -8,11 +8,19 @@ def softplus_np(x):
     return np.log(np.exp(x) + 1) - np.log(2.0)
 
 
+def swish_np(x):
+    return x / (1 + np.exp(-x))
+
+
 class TestSP(unittest.TestCase):
     def test_softplus(self):
         x = 10.0
         self.assertAlmostEqual(softplus2(x).numpy(), softplus_np(x), places=5)
 
+    def test_swish(self):
+
+        self.assertAlmostEqual(swish(10.0).numpy(), swish_np(10.0), places=5)
+        
 
 if __name__ == "__main__":
     unittest.main()
